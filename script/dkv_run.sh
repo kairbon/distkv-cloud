7d3d9d936d4426a097eecf42bef98f5aa90b36d8
karbon <wangkai31415926@gmail.com>
2020-03-07 14:28:55 +0800

    fix

--- script/dkv_run.sh
+++ script/dkv_run.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 cp ../distkv_tmp/server/target/distkv-server-0.1.4-SNAPSHOT-jar-with-dependencies.jar ./distkv_server.jar
-cp ../target/distkv-cloud-0.1.4-SNAPSHOT-jar-with-dependencies.jar ./distkv_cloud.jar
+cp ../target/distkv-cloud-0.1.0-SNAPSHOT-jar-with-dependencies.jar ./distkv_cloud.jar
 cp ../src/main/java/com/distkv/tool/dashboard/index.html ./index.html
 
 nohup java -cp ./distkv_server.jar com.distkv.server.storeserver.StoreServer >> /dev/null 2>&1 &
